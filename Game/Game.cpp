9165cf2b80e13810198bddf876489b103a129087
yinnis <12302157@student.pxl.be>
2024-09-19 16:04:08 +0200

    Kleine Game Test

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -2,16 +2,23 @@
 #include "SFML\Graphics.hpp"
 #include "SFML\Window.hpp"
 #include "SFML\System.hpp"
+#include <cstdlib>
 
 using namespace sf; //hoef ik niet overal sf:: voor te zetten.
 
+void Update(RectangleShape &square, RenderWindow& window);
+void Draw(RenderWindow &window, RectangleShape &square);
+
 int main()
 {
-    RenderWindow window(VideoMode(1920, 1080), "Game V1.0");
+    VideoMode desktop = VideoMode::getDesktopMode();
+    RenderWindow window(desktop, "Game V1.0", Style::None);
     window.setFramerateLimit(60);
+    window.setVerticalSyncEnabled(true);
 
-    CircleShape circle(20.f);
-    circle.setFillColor(Color::Blue);
+    RectangleShape square(Vector2f(100.f, 100.f));
+    square.setFillColor(Color::Red);
+    square.setPosition(window.getSize().x / 2, window.getSize().x / 2);
 
 
     while (window.isOpen())
@@ -21,20 +28,52 @@ int main()
         {
             if (event.type == Event::Closed)
                 window.close();
+
+            if (event.KeyPressed && event.key.code == Keyboard::Escape)
+                window.close();
         }
 
-        //Update
-        
-
-        //Clear : Moet gebeuren vor telkens een nieuwe versie van de display te krijgen.
-        window.clear(); 
-
-        //Draw the updated version.
-        window.draw(circle);
-     
-        //Display the updated version.
-        window.display();
+        Update(square, window);
+        Draw(window, square);
+ 
     }
 
     return 0;
+}
+
+void Update(RectangleShape &square, RenderWindow& window)
+{
+    if ( Keyboard::isKeyPressed(Keyboard::Q) && square.getPosition().x > 0 )
+    {
+        square.move(-10.f, 0.f);
+    }
+    if ( Keyboard::isKeyPressed(Keyboard::D) && square.getPosition().x + square.getSize().x < window.getSize().x )
+    {
+        square.move(10.f, 0.f);
+    }
+    if ( Keyboard::isKeyPressed(Keyboard::Z) && square.getPosition().y > 0 )
+    {
+        square.move(0.f, -10.f);
+    }
+    if ( Keyboard::isKeyPressed(Keyboard::S) && square.getPosition().y + square.getSize().y < window.getSize().y )
+    {
+        square.move(0.f, 10.f);
+    }
+
+    if (Mouse::isButtonPressed(Mouse::Left))
+    {
+        square.setFillColor(Color::Blue);
+    }
+    else
+    {
+        square.setFillColor(Color::Red);
+    }
+}
+
+
+void Draw(RenderWindow& window, RectangleShape &square)
+{
+    window.clear(Color::Cyan); //Clear : Moet gebeuren vor telkens een nieuwe versie van de display te krijgen.
+    window.draw(square);       //Draw the updated version.
+    window.display();          //Display the updated version.
 }
